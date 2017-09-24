f888506f8caa6929cb2fc3805653775a8f2e7389
Vladislav <vladnekrash@ya.ru>
2017-09-24 23:27:08 +0300

    Fibonacci Solution

--- 01/tasks.js
+++ 01/tasks.js
@@ -15,8 +15,15 @@ function getMinMax(string) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
+
 function fibonacciSimple(x) {
-  return x;
+	if (x===1){
+	    return 1;
+	}
+	if (x===0){
+		return 0;
+	}
+  return fibonacciSimple(x-1)+fibonacciSimple(x-2);
 }
 
 /* ============================================= */
